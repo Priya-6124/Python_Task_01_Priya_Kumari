6fe718c979009d248f757b3fe9013951aa3663b4
Priya-6124 <25mcab38@kristujayanti.com>
2026-06-06 14:10:30 +0530

    Add files via upload

---
+++ system_info.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+name = "Priya Kumari"
+college = "Kristu Jayanti Institute of Technology"
+branch = "MCA"
+internship = "Python Programming Summer Internship"
+
+current_time = datetime.now()
+
+print("Name:", name)
+print("College:", college)
+print("Branch:", branch)
+print("Internship:", internship)
+print("Date & Time:", current_time)
